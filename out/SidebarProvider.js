a34a6dbfdba311e5d57e156b524e8d6e22f9fc44
adens <asepindrakurniawan@gmail.com>
2024-11-05 03:27:15 +0700

    fix: chat

--- out/SidebarProvider.js
+++ out/SidebarProvider.js
@@ -1,6 +1,31 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SidebarProvider = void 0;
+const path = __importStar(require("path"));
+const fs = __importStar(require("fs"));
 class SidebarProvider {
     _extensionUri;
     _view;
@@ -16,33 +41,9 @@ class SidebarProvider {
         webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
     }
     getHtmlForWebview(webview) {
-        return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pacar AI Chat</title>
-        <style>
-          body, html {
-            width: 100%;
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-          }
-          iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
-          }
-        </style>
-      </head>
-      <body>
-        <iframe src="https://chat.pacar-ai.my.id/"></iframe>
-      </body>
-      </html>
-    `;
+        const htmlPath = path.join(this._extensionUri.fsPath, 'media', 'webview.html');
+        const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+        return htmlContent;
     }
 }
 exports.SidebarProvider = SidebarProvider;
--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -1,52 +1,30 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
 
 export class SidebarProvider implements vscode.WebviewViewProvider {
-    private _view?: vscode.WebviewView;
+  private _view?: vscode.WebviewView;
 
-    constructor(private readonly _extensionUri: vscode.Uri) { }
+  constructor(private readonly _extensionUri: vscode.Uri) { }
 
-    public resolveWebviewView(
-        webviewView: vscode.WebviewView,
-        context: vscode.WebviewViewResolveContext,
-        _token: vscode.CancellationToken
-    ) {
-        this._view = webviewView;
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
 
-        webviewView.webview.options = {
-            enableScripts: true,
-            localResourceRoots: [this._extensionUri]
-        };
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri]
+    };
 
-        webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
-    }
+    webviewView.webview.html = this.getHtmlForWebview(webviewView.webview);
+  }
 
-    private getHtmlForWebview(webview: vscode.Webview): string {
-        return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Pacar AI Chat</title>
-        <style>
-          body, html {
-            width: 100%;
-            height: 100%;
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-          }
-          iframe {
-            width: 100%;
-            height: 100%;
-            border: none;
-          }
-        </style>
-      </head>
-      <body>
-        <iframe src="https://chat.pacar-ai.my.id/"></iframe>
-      </body>
-      </html>
-    `;
-    }
+  private getHtmlForWebview(webview: vscode.Webview): string {
+    const htmlPath = path.join(this._extensionUri.fsPath, 'media', 'webview.html');
+    const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+    return htmlContent;
+  }
 }
